6f72946b652755d5637cee8c14f2a90c4845617f
markalaugala775-ops <markalaugala775@gmail.com>
2026-09-08 10:56:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/wipe-trust-cybersecurity/',
   plugins: [react()],
   resolve: {
     alias: {
